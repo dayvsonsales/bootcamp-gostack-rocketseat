265416f3bc8ab171a82ca2eecc971fdacd13740c
Dayvson Sales <dayvson@outlook.com>
2020-02-25 15:04:13 -0300

    Add second phase

---
+++ modulo02/src/routes.js
@@ -0,0 +1,41 @@
+import { Router } from 'express';
+import multer from 'multer';
+
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+import AppointmentController from './app/controllers/AppointmentController';
+import ProviderController from './app/controllers/ProviderController';
+import ScheduleController from './app/controllers/ScheduleController';
+import NotificationController from './app/controllers/NotificationController';
+import AvailableController from './app/controllers/AvailableController';
+
+import multerConfig from './config/multer';
+
+import authMiddleware from './app/middleware/auth';
+
+const routes = new Router();
+const upload = multer(multerConfig);
+
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
+
+routes.get('/providers', ProviderController.index);
+routes.get('/providers/:providerId/available', AvailableController.index);
+
+routes.get('/appointments', AppointmentController.index);
+routes.post('/appointments', AppointmentController.store);
+routes.delete('/appointments/:id', AppointmentController.destroy);
+
+routes.get('/schedule', ScheduleController.index);
+
+routes.get('/notifications', NotificationController.index);
+routes.put('/notifications', NotificationController.update);
+
+routes.post('/files', upload.single(), FileController.store);
+
+export default routes;
