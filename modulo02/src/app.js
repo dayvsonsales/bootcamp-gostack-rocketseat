ad468861862eb136fb89731bd77917bc3cea245e
Dayvson Sales <dayvson@outlook.com>
2020-03-24 21:29:17 -0300

    Second module: add cors

--- modulo02/src/app.js
+++ modulo02/src/app.js
@@ -3,6 +3,7 @@ import 'dotenv/config';
 import express from 'express';
 import path from 'path';
 import * as Sentry from '@sentry/node';
+import cors from 'cors';
 import Youch from 'youch';
 import 'express-async-errors';
 import routes from './routes';
@@ -27,6 +28,7 @@ class App {
   middlewares() {
     this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
+    this.server.use(cors());
     this.server.use(
       '/files',
       express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
