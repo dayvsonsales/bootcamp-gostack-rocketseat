70ec3318cb51adefe7b4e1a5cec8b9954c7160bc
Dayvson Sales <dayvson@outlook.com>
2020-03-24 21:31:36 -0300

    Second module: schedule now include User and fixed return

--- modulo02/src/app/controllers/ScheduleController.js
+++ modulo02/src/app/controllers/ScheduleController.js
@@ -29,10 +29,17 @@ class ScheduleController {
           [Op.between]: [startOfDay(parsedDate), endOfDay(parsedDate)],
         },
       },
+      include: [
+        {
+          model: User,
+          as: 'user',
+          attributes: ['id', 'name'],
+        },
+      ],
       order: ['date'],
     });
 
-    return res.json({ appointments });
+    return res.json(appointments);
   }
 }
 
