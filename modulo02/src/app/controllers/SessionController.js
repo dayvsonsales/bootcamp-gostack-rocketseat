8fd2c4cd9591a98ca603567ccba6560466f1e233
Dayvson Sales <dayvson@outlook.com>
2020-03-24 21:32:00 -0300

    Second module: includes files in query

--- modulo02/src/app/controllers/SessionController.js
+++ modulo02/src/app/controllers/SessionController.js
@@ -4,6 +4,7 @@ import * as Yup from 'yup';
 import authConfig from '../../config/auth';
 
 import User from '../models/User';
+import File from '../models/File';
 
 class SessionController {
   async store(req, res) {
@@ -20,23 +21,30 @@ class SessionController {
 
     const { email, password } = req.body;
 
-    const user = await User.findOne({ where: { email } });
+    const user = await User.findOne({
+      where: { email },
+      include: [
+        { model: File, as: 'avatar', attributes: ['id', 'path', 'url'] },
+      ],
+    });
 
     if (!user) {
-      return res.status(401).json({ error: 'User not found!' });
+      return res.status(404).json({ error: 'User not found!' });
     }
 
     if (!(await user.checkPassword(password))) {
       return res.status(401).json({ error: 'Password does not match' });
     }
 
-    const { id, name } = user;
+    const { id, name, avatar, provider } = user;
 
     return res.json({
       user: {
         id,
         name,
         email,
+        avatar,
+        provider,
       },
       token: jwt.sign({ id }, authConfig.secret, {
         expiresIn: authConfig.expiresIn,
