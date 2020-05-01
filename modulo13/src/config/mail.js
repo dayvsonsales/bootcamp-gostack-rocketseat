e8a258de3ff350b2b4e174dd433cafed8d5c2a59
Dayvson Sales <dayvson@outlook.com>
2020-04-30 23:25:59 -0300

    Update mail.js

--- modulo13/src/config/mail.js
+++ modulo13/src/config/mail.js
@@ -3,8 +3,8 @@ export default {
   port: 2525,
   secure: false,
   auth: {
-    user: 'ccc03bbaefcd63',
-    pass: '41cf6f8bd98c6e',
+    user: '',
+    pass: '',
   },
   default: {
     from: 'Equipe Gobarber <noreply@gobarber.com>',
